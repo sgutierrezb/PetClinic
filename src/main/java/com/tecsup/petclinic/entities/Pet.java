f6a7399cebcb0be51be682d97ac50fd36c698814
miafernandez14 <mia.fernandez@tecsup.edu.pe>
2022-11-07 15:28:14 -0500

    2do commit lab 12

--- src/main/java/com/tecsup/petclinic/entities/Pet.java
+++ src/main/java/com/tecsup/petclinic/entities/Pet.java
@@ -1,12 +1,16 @@
 package com.tecsup.petclinic.entities;
 
-import java.sql.Date;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 /**
  * 
@@ -24,24 +28,31 @@ public class Pet {
 	private int typeId;
 	@Column(name = "owner_id")
 	private int ownerId;
-	private Date birth_date;
+	
+	@Column(name = "birth_date")
+	//@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd",  timezone = "GMT+8")
+	private Date birthDate;
 	
 	public Pet() {
 	}
 
-	public Pet(long id, String name, int type_id, int owner_id) {
+	public Pet(long id, String name, int type_id, int owner_id, Date birth_date) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.typeId = type_id;
 		this.ownerId = owner_id;
+		this.birthDate = birth_date;
 	}
 
-	public Pet(String name, int type_id, int owner_id) {
+	public Pet(String name, int type_id, int owner_id, Date birth_date) {
 		super();
 		this.name = name;
 		this.typeId = type_id;
 		this.ownerId = owner_id;
+		this.birthDate = birth_date;
+
 	}
 
 	public long getId() {
@@ -78,8 +89,16 @@ public class Pet {
 
 	@Override
 	public String toString() {
-		return "Pet [id=" + id + ", name=" + name + ", typeId=" + typeId + ", ownerId=" + ownerId + ", birth_date="
-				+ birth_date + "]";
+		return "Pet [id=" + id + ", name=" + name + ", typeId=" + typeId + ", ownerId=" + ownerId + ", birthDate="
+				+ birthDate + "]";
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
 	}
 
 
